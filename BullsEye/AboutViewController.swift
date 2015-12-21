71da0097b681b075a1f6e1d798a2bdd368d62856
xiawufrance <veronique@Mac-mini-de-Veronique.local>
2015-12-21 16:10:23 +0100

    first commit

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -1,35 +1,27 @@
-//
-//  AboutViewController.swift
-//  BullsEye
-//
-//  Created by M.I. Hollemans on 23/07/15.
-//  Copyright © 2015 Razeware. All rights reserved.
-//
-
 import UIKit
 
+// This is the About screen. It shows the gameplay instructions in a web view.
+// It also has a Close button that closes the screen and returns the player to
+// the main game screen.
 class AboutViewController: UIViewController {
+  @IBOutlet weak var webView: UIWebView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Load the BullsEye.html file into the web view.
+    if let htmlFile = NSBundle.mainBundle().pathForResource("BullsEye", ofType: "html") {
+      if let htmlData = NSData(contentsOfFile: htmlFile) {
+        let baseURL = NSURL(fileURLWithPath: NSBundle.mainBundle().bundlePath)
+        webView.loadData(htmlData, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+      }
     }
-    */
+  }
 
+  // This action is called when the user taps the Close button. In response, we
+  // dismiss the About screen and automatically return to the main game screen
+  // (BullsEyeViewController).
+  @IBAction func close() {
+    dismissViewControllerAnimated(true, completion: nil)
+  }
 }
